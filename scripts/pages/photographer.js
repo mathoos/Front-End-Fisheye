b6159fc7b5ae32f3dddb3ef6da61e972868584a7
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2022-10-13 00:46:11 +0200

    factorisation

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -55,203 +55,54 @@ class Photographers {
     wrapper.innerHTML = photographerHeader;
     return wrapper;
   }
-  }
 
-  createMediaWrapper() {
+  createBottomWrapper(){
     const wrapper = document.createElement('div');
-    wrapper.classList.add('medias_card');
+    wrapper.classList.add('likes_container');
 
     const mediaWrapper =
-        `
-        <div class="medias_card-up">${this.mediaWrapper.displayMedia()}</div>
-        <div class="medias_card-down">
-            <div class="medias_card-down--title">
-                <p>${this.mediaWrapper.title}</p>
-            </div>
-            <div class="medias_card-down--likes">
-                <p>${this.mediaWrapper.likes}</p>
-                <button class="like">
-                    <i class="visible-heart far fa-heart"></i>
-                </button>
-            </div>
-        </div>
-        `;
+    `
+    <p class="likes_price">${this.photographers.price}€/jour</p>
+
+    `;
 
     wrapper.innerHTML = mediaWrapper;
     return wrapper;
   }
 }
 
-class Image extends Media {
-  constructor(media) {
-    super(media);
-  }
 
-  displayMedia() {
-    return `<img alt="${this.mediaWrapper.title}" class="displayMedia" id="image" src="assets/media/${this.mediaWrapper.image}">`;
-  }
-}
-
-class Video extends Media {
-  constructor(media) {
-    super(media); // appel du constructeur parent
-  }
-
-  displayMedia() {
-    return `<video class="displayMedia" id="image" autoplay src="assets/media/${this.mediaWrapper.video}" type="video/mp4"></video>`;
-  }
-}
-
-
-class MediaFactory {
-  static create(media) {
-    if (media.image) {
-      return new Image(media);
-    } else if (media.video) {
-      return new Video(media);
-    } else {
-      throw 'Unknown media';
-    }
-  }
-}
-
-
-class AppMedia {
+class PhotographerHeader {
   constructor() {
-    this.MediasWrapper = document.querySelector('.medias');
+    this.photographersHeader = document.querySelector('.photographer_header');
   }
 
-  async mainMedia() {
-    const mediasData = await getMedias();
-    let mediaFilter = mediasData.filter((media) => media.photographerId == photographerId);
-    mediaFilter = mediaFilter.map((media) => MediaFactory.create(media));
-
-    // AFFICHER MEDIAS
-    mediaFilter.forEach((mediaWrapper) => {
-      const template = new Media(mediaWrapper);
-      this.MediasWrapper.appendChild(
-          template.createMediaWrapper(),
-
-      );
-    });
-  }
-
-  async sort() {
-    const mediasData = await getMedias();
-    let mediaFilter = mediasData.filter((media) => media.photographerId == photographerId);
-    mediaFilter = mediaFilter.map((media) => MediaFactory.create(media));
-
-    const select = document.getElementById('sort-select');
-
-    select.addEventListener('change', (e) => {
-      e.preventDefault();
-      const sortType = select.options[select.selectedIndex].id;
-
-      // TRI PAR DATE
-      if (sortType === 'dateButton') {
-        console.log('Tri par date');
-        mediaFilter.sort(dateFilter);
-        document.querySelector('.medias').innerHTML = '';
-        mediaFilter.forEach((mediaWrapper) => {
-          const template = new Media(mediaWrapper);
-          this.MediasWrapper.appendChild(
-              template.createMediaWrapper(),
-
-          );
-        });
-      }
-
-      // TRI PAR POPULARITE
-      else if (sortType === 'popularityButton') {
-        console.log('tri par popularité');
-        mediaFilter.sort(popularityFilter);
-
-        document.querySelector('.medias').innerHTML = '';
-        mediaFilter
-            .forEach((mediaWrapper) => {
-              const Template = new Media(mediaWrapper);
-              this.MediasWrapper.appendChild(
-                  Template.createMediaWrapper(),
-              );
-            });
-      }
-
-      // TRI PAR TITRE
-      else if (sortType === 'titleButton') {
-        console.log('tri par titre');
-        mediaFilter.sort(titleFilter);
-
-        document.querySelector('.medias').innerHTML = '';
-        mediaFilter
-            .forEach((mediaWrapper) => {
-              const Template = new Media(mediaWrapper);
-              this.MediasWrapper.appendChild(
-                  Template.createMediaWrapper(),
-              );
-            });
-      }
-    });
+  async main() {
+    const photographersData = await getPhotographers();
+    const photographerFilter = photographersData.filter((photographer) => photographer.id == photographerId);
+    photographerFilter.forEach((header) => {
+        this.photographersHeader.appendChild(new Photographers(header).createPhotographerHeader()
+        )
+    })
   }
 }
-const appMedia = new AppMedia();
-appMedia.mainMedia();
-appMedia.sort();
 
-
-// FILTRE
-
-
-function dateFilter(a, b) {
-  if (a.mediaWrapper.date < b.mediaWrapper.date) {
-    return -1;
+class WrapperBottom{
+  constructor() {
+    this.MediasBottom = document.querySelector('.likes');
   }
-  if (a.mediaWrapper.date > b.mediaWrapper.date) {
-    return 1;
+   async main() {
+    const photographersData = await getPhotographers();
+    const photographerFilter = photographersData.filter((photographer) => photographer.id == photographerId);
+
+    photographerFilter.forEach((price) => {
+        this.MediasBottom.appendChild(new Photographers(price).createBottomWrapper()
+        )
+    })
   }
-  return 0;
 }
 
-function popularityFilter(a, b) {
-  if (a.mediaWrapper.likes > b.mediaWrapper.likes) {
-    return -1;
-  }
-  if (a.mediaWrapper.likes < b.mediaWrapper.likes) {
-    return 1;
-  }
-  return 0;
-}
+new PhotographerHeader().main()
+new WrapperBottom().main()
 
-function titleFilter(a, b) {
-  if (a.mediaWrapper.title < b.mediaWrapper.title) {
-    return -1;
-  }
-  if (a.mediaWrapper.title > b.mediaWrapper.title) {
-    return 1;
-  }
-  return 0;
-}
-
-
-// AFFICHER LE TOTAL DE LIKES
-
-async function totalLikes() {
-  const mediasData = await getMedias();
-  const mediaFilter = mediasData.filter((media) => media.photographerId == photographerId);
-
-
-  const likes = mediaFilter.map((total) => total.likes);
-  const initialValue = 0;
-  const totalLikes = likes.reduce((previousValue, currentValue) => previousValue + currentValue, initialValue);
-  document.querySelector('.likes').innerHTML +=
-    `<div class="likes_bloc">
-        <h1 class="likes_bloc-total">${totalLikes}</h1>
-        <i class="fa-solid fa-heart"></i>
-     </div>
-    `;
-
-  // INCREMENTE NOMBRE DE LIKES AU CLICK
-  let totalOfLikes = parseInt(document.querySelector('.likes_bloc-total').innerText);
-  const likesArray = Array.from(document.querySelectorAll('.like')); // Créé un tableau de tous les <p class="jaime"></p>
-  likesArray.forEach((jaime) => { // boucle à travers chaque .jaime
-    let liked = false;
 
