ff0329cf2c53baa2bea112476bcee7fb6b6835bd
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2022-10-12 18:39:29 +0200

    sass + eslint install

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,7 +1,6 @@
-
 class App {
     constructor() {
-        this.photographersWrapper = document.querySelector('.photographers_wrapper')
+        this.photographersWrapper = document.querySelector('.photographers')
     }
 
     async main() {
@@ -13,7 +12,7 @@ class App {
             // Ici, je transforme mon tableau de données en un tableau de classe Movie
             .map(photographers => new Photographers(photographers))
             .forEach(card => {
-                const Template = new photographersCard(card)
+                const Template = new PhotographersCard(card)
                 this.photographersWrapper.appendChild(
                     Template.createPhotographerCard()
                 )
@@ -44,22 +43,22 @@ class Photographers{
 }
 
 
-class photographersCard {
+class PhotographersCard {
     constructor(card) {
         this.card = card
     }
 
     createPhotographerCard() {
         const wrapper = document.createElement('article')
-        wrapper.classList.add('photographer-card-wrapper')
+        wrapper.classList.add('photographers_card')
 
         const photographerCard = 
         `
-            <a href="photographer.html?id=${this.card.id}" alt="${this.card.name}">
+            <a class="photographers_card-up" href="photographer.html?id=${this.card.id}" alt="${this.card.name}">
                 <img src="assets/photographers/${this.card.portrait}">
                 <h2>${this.card.name}</h2>
             </a>
-            <div>
+            <div class="photographers_card-down">
                 <p class="city">${this.card.city}, ${this.card.country}</p>
                 <p class="tagline">${this.card.tagline}</p>
                 <p class="price">${this.card.price}€/jour</p>
@@ -71,57 +70,3 @@ class photographersCard {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Récupérer les données json
-/*async function getData(){
-    try{
-        var response = await fetch('data/photographers.json');    
-        var data = await response.json();
-        return data;       
-    }
-    catch(e){
-        console.log(e)
-    }    
-}
-
-getData()
-*/
-
-
-
-// CODE A REPLACER EN HAUT 
-/* async function displayData() {
-    var response = await fetch('data/photographers.json'); 
-    var data = await response.json();
-    var photographers = await data.photographers;
-
-    photographers.forEach((photographer =>{
-        document.getElementById("photographer_section").innerHTML +=
-        `<article>
-            <a href="photographer.html?id=${photographer.id}" alt="${photographer.name}">
-                <img src="assets/photographers/${photographer.portrait}">
-                <h2>${photographer.name}</h2>
-            </a>
-            <div>
-                <p class="city">${photographer.city}, ${photographer.country}</p>
-                <p class="tagline">${photographer.tagline}</p>
-                <p class="price">${photographer.price}€/jour</p>
-            </div>
-        </article>`
-    }))   
-}
-
-displayData()
-*/
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,288 +1,312 @@
-//Obtenir l'id de l'url
+// Obtenir l'id de l'url
+
 const photographerUrl = window.location.search;
 const urlParams = new URLSearchParams(photographerUrl);
-const photographerId = urlParams.get("id");
-
-
+const photographerId = urlParams.get('id');
 
 
 async function getPhotographers() {
-    try{
-        var response = await fetch('data/photographers.json');    
-        var data = await response.json();
-        const photographersData = await data.photographers;
-        return photographersData
-        
-    }
-    catch(e){
-        console.log(e)
-    }   
+  try {
+    const response = await fetch('data/photographers.json');
+    const data = await response.json();
+    const photographersData = await data.photographers;
+    return photographersData;
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 async function getMedias() {
-    try{
-        var response = await fetch('data/photographers.json');    
-        var data = await response.json();
-        const mediasData = await data.media;
-        return mediasData
-        
-    }
-    catch(e){
-        console.log(e)
-    }   
+  try {
+    const response = await fetch('data/photographers.json');
+    const data = await response.json();
+    const mediasData = await data.media;
+    return mediasData;
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 
-class photographersHeader {
-    constructor(header) {
-        this.header = header
-    }
+class PhotographersHeader {
+  constructor(header) {
+    this.header = header;
+  }
 
-    createPhotographerHeader() {
-        const wrapper = document.createElement('div')
-        wrapper.classList.add('photographers_header')
+  createPhotographerHeader() {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('photographer');
 
-        const photographerHeader = 
+    const photographerHeader =
         `
-        <div class="photographers_header--bloc">
+        <div class="photographer_info">
             <h1>${this.header.name}</h1>
             <p class="city">${this.header.city}, ${this.header.country}</p>
             <p class="tagline">${this.header.tagline}</p>
         </div>
-        <div class="photographers_header--bloc">
+        <div class="photographer_contact">
             <button class="contact_button" alt="Contact Me" onclick="displayModal()">Contactez-moi</button>
         </div>  
-        <div class="photographers_header--bloc">
+        <div class="photographer_photo">
             <img src="assets/photographers/${this.header.portrait}" alt ="${this.header.name}">
         </div>
-        `
+        `;
 
-        document.getElementById("likes").innerHTML +=
-        `<p>${this.header.price}€/jour</p>` 
+    document.querySelector('.likes').innerHTML =
+        `<p class="likes_price">${this.header.price}€/jour</p>`;
 
-        wrapper.innerHTML = photographerHeader
-        return wrapper
-    }
+    wrapper.innerHTML = photographerHeader;
+    return wrapper;
+  }
 }
 
 
 class App {
-    constructor() {
-        this.photographersWrapper = document.querySelector('.photograph_header')
-    }
+  constructor() {
+    this.photographersWrapper = document.querySelector('.photographer_header');
+  }
 
-    async main() {
-        const photographersData = await getPhotographers()  
-        const photographerFilter = photographersData.filter((photographer) => photographer.id == photographerId);
-        photographerFilter             
-            .forEach((header) => {
-                const Template = new photographersHeader(header)
-                this.photographersWrapper.appendChild(
-                Template.createPhotographerHeader()
-            )
-        })         
-    }
+  async main() {
+    const photographersData = await getPhotographers();
+    const photographerFilter = photographersData.filter((photographer) => photographer.id == photographerId);
+    photographerFilter
+        .forEach((header) => {
+          const Template = new PhotographersHeader(header);
+          this.photographersWrapper.appendChild(
+              Template.createPhotographerHeader(),
+          );
+        });
+  }
 }
 
-const app = new App()
-app.main()
+const app = new App();
+app.main();
 
 
 // MEDIA
 
 class Media {
-    constructor(media) {
-        this.id = media.id
-        this.photographerId = media.photographerId
-        this.title = media.title
-        this.likes = media.likes
-        this.date = media.date
-        this.price = media.price
-    }  
-}
+  constructor(mediaWrapper) {
+    this.mediaWrapper = mediaWrapper;
+    this.likes = mediaWrapper.likes;
+    this.title = mediaWrapper.title;
+    this.image = mediaWrapper.image;
+  }
 
-class MediaWrapper {
-    constructor(mediaWrapper) {
-        this.media = mediaWrapper
-    }
+  createMediaWrapper() {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('medias_card');
 
-    createMediaWrapper() {
-        const wrapper = document.createElement('div')
-        wrapper.classList.add('media-wrapper')
-
-        const mediaWrapper = 
+    const mediaWrapper =
         `
-        <div class="media-wrapper_up">${this.media.displayMedia()}</div>
-        <div class="media-wrapper_down">
-            <div class="media-wrapper_down--title">
-                <p>${this.media.title}</p>
+        <div class="medias_card-up">${this.mediaWrapper.displayMedia()}</div>
+        <div class="medias_card-down">
+            <div class="medias_card-down--title">
+                <p>${this.mediaWrapper.title}</p>
             </div>
-            <div class="media-wrapper_down--likes">
-                <p>${this.media.likes}</p>
-                <button class="jaime"><i class="fa-regular fa-heart"></i></button>
+            <div class="medias_card-down--likes">
+                <p>${this.mediaWrapper.likes}</p>
+                <button class="like">
+                    <i class="visible-heart far fa-heart"></i>
+                </button>
             </div>
         </div>
-        `
-        
-        wrapper.innerHTML = mediaWrapper
-        return wrapper
+        `;
+
+    wrapper.innerHTML = mediaWrapper;
+    return wrapper;
+  }
+}
+
+class Image extends Media {
+  constructor(media) {
+    super(media);
+  }
+
+  displayMedia() {
+    return `<img alt="${this.mediaWrapper.title}" class="displayMedia" id="image" src="assets/media/${this.mediaWrapper.image}">`;
+  }
+}
+
+class Video extends Media {
+  constructor(media) {
+    super(media); // appel du constructeur parent
+  }
+
+  displayMedia() {
+    return `<video class="displayMedia" id="image" autoplay src="assets/media/${this.mediaWrapper.video}" type="video/mp4"></video>`;
+  }
+}
+
+
+class MediaFactory {
+  static create(media) {
+    if (media.image) {
+      return new Image(media);
+    } else if (media.video) {
+      return new Video(media);
+    } else {
+      throw 'Unknown media';
     }
+  }
 }
 
 
 class AppMedia {
-    constructor() {
-        this.MediasWrapper = document.querySelector('.medias')
-    }
+  constructor() {
+    this.MediasWrapper = document.querySelector('.medias');
+  }
 
-    async mainMedia() {
-        const mediasData = await getMedias()
-        let mediaFilter = mediasData.filter((media) => media.photographerId == photographerId);
-        mediaFilter = mediaFilter.map((media) => MediaFactory.create(media))
+  async mainMedia() {
+    const mediasData = await getMedias();
+    let mediaFilter = mediasData.filter((media) => media.photographerId == photographerId);
+    mediaFilter = mediaFilter.map((media) => MediaFactory.create(media));
 
-        // AFFICHER MEDIAS             
-        mediaFilter.forEach((mediaWrapper) => {               
-            const template = new MediaWrapper(mediaWrapper)
-                this.MediasWrapper.appendChild(
-                template.createMediaWrapper()              
-            )
-        })     
-    }
+    // AFFICHER MEDIAS
+    mediaFilter.forEach((mediaWrapper) => {
+      const template = new Media(mediaWrapper);
+      this.MediasWrapper.appendChild(
+          template.createMediaWrapper(),
+
+      );
+    });
+  }
+
+  async sort() {
+    const mediasData = await getMedias();
+    let mediaFilter = mediasData.filter((media) => media.photographerId == photographerId);
+    mediaFilter = mediaFilter.map((media) => MediaFactory.create(media));
+
+    const select = document.getElementById('sort-select');
+
+    select.addEventListener('change', (e) => {
+      e.preventDefault();
+      const sortType = select.options[select.selectedIndex].id;
+
+      // TRI PAR DATE
+      if (sortType === 'dateButton') {
+        console.log('Tri par date');
+        mediaFilter.sort(dateFilter);
+        document.querySelector('.medias').innerHTML = '';
+        mediaFilter.forEach((mediaWrapper) => {
+          const template = new Media(mediaWrapper);
+          this.MediasWrapper.appendChild(
+              template.createMediaWrapper(),
+
+          );
+        });
+      }
+
+      // TRI PAR POPULARITE
+      else if (sortType === 'popularityButton') {
+        console.log('tri par popularité');
+        mediaFilter.sort(popularityFilter);
+
+        document.querySelector('.medias').innerHTML = '';
+        mediaFilter
+            .forEach((mediaWrapper) => {
+              const Template = new Media(mediaWrapper);
+              this.MediasWrapper.appendChild(
+                  Template.createMediaWrapper(),
+              );
+            });
+      }
+
+      // TRI PAR TITRE
+      else if (sortType === 'titleButton') {
+        console.log('tri par titre');
+        mediaFilter.sort(titleFilter);
+
+        document.querySelector('.medias').innerHTML = '';
+        mediaFilter
+            .forEach((mediaWrapper) => {
+              const Template = new Media(mediaWrapper);
+              this.MediasWrapper.appendChild(
+                  Template.createMediaWrapper(),
+              );
+            });
+      }
+    });
+  }
 }
-const appMedia = new AppMedia()
-appMedia.mainMedia()
+const appMedia = new AppMedia();
+appMedia.mainMedia();
+appMedia.sort();
 
-class Sort{
-    constructor() {
-        this.MediasWrapper = document.querySelector('.medias')
-    }
-    async sort(){
 
-        const mediasData = await getMedias()
-        let mediaFilter = mediasData.filter((media) => media.photographerId == photographerId);
-        mediaFilter = mediaFilter.map((media) => MediaFactory.create(media))
-        const select = document.getElementById("sort-select");
-    
-        select.addEventListener("change", (e) => {
-            e.preventDefault()
-            let sortType = select.options[select.selectedIndex].id;
-    
-            // TRI PAR DATE
-            if(sortType === "dateButton"){
-                console.log("Tri par date")
-                mediaFilter.sort(dateFilter);          
-                document.querySelector(".medias").innerHTML = "";
-                mediaFilter
-                .forEach((mediaWrapper) => {
-                    const Template = new MediaWrapper(mediaWrapper)
-                        this.MediasWrapper.appendChild(
-                        Template.createMediaWrapper()
-                    )
-                })                             
-            }
-    
-            // TRI PAR POPULARITE
-            else if(sortType === "popularityButton"){
-                console.log("tri par popularité")
-                mediaFilter.sort(popularityFilter);
-                
-                document.querySelector(".medias").innerHTML = "";
-                mediaFilter
-                .forEach((mediaWrapper) => {
-                    const Template = new MediaWrapper(mediaWrapper)
-                        this.MediasWrapper.appendChild(
-                        Template.createMediaWrapper()
-                    )
-                })               
-            }
-    
-            // TRI PAR TITRE
-            else if(sortType === "titleButton"){
-                console.log("tri par titre")
-                mediaFilter.sort(titleFilter); 
-                
-                document.querySelector(".medias").innerHTML = "";
-                mediaFilter
-                .forEach((mediaWrapper) => {
-                    const Template = new MediaWrapper(mediaWrapper)
-                        this.MediasWrapper.appendChild(
-                        Template.createMediaWrapper()
-                    )
-                })       
-            }
-        })
-    }
-}
-
-const appSort = new Sort()
-appSort.sort()
+// FILTRE
 
 
 function dateFilter(a, b) {
-    if (a.date < b.date) {
+  if (a.mediaWrapper.date < b.mediaWrapper.date) {
     return -1;
-    }
-    if (a.date > b.date) {
+  }
+  if (a.mediaWrapper.date > b.mediaWrapper.date) {
     return 1;
-    }
-    return 0;
+  }
+  return 0;
 }
+
 function popularityFilter(a, b) {
-    if (a.likes > b.likes) {
+  if (a.mediaWrapper.likes > b.mediaWrapper.likes) {
     return -1;
-    }
-    if (a.likes < b.likes) {
+  }
+  if (a.mediaWrapper.likes < b.mediaWrapper.likes) {
     return 1;
-    }
-    return 0;
+  }
+  return 0;
 }
+
 function titleFilter(a, b) {
-    if (a.title < b.title) {
+  if (a.mediaWrapper.title < b.mediaWrapper.title) {
     return -1;
-    }
-    if (a.title > b.title) {
+  }
+  if (a.mediaWrapper.title > b.mediaWrapper.title) {
     return 1;
-    }
-    return 0;
+  }
+  return 0;
 }
+
+
 // AFFICHER LE TOTAL DE LIKES
 
-async function totalLikes(){
-    const mediasData = await getMedias()
-    let mediaFilter = mediasData.filter((media) => media.photographerId == photographerId);
+async function totalLikes() {
+  const mediasData = await getMedias();
+  const mediaFilter = mediasData.filter((media) => media.photographerId == photographerId);
 
 
-    var likes = mediaFilter.map(total => total.likes)
-    const initialValue = 0;
-    const totalLikes = likes.reduce((previousValue, currentValue) => previousValue + currentValue, initialValue);
-    document.getElementById("likes").innerHTML +=
-    `<h1 id="total-likes">${totalLikes}<i class="fa-solid fa-heart"></i></h1>
-    ` 
+  const likes = mediaFilter.map((total) => total.likes);
+  const initialValue = 0;
+  const totalLikes = likes.reduce((previousValue, currentValue) => previousValue + currentValue, initialValue);
+  document.querySelector('.likes').innerHTML +=
+    `<div class="likes_bloc">
+        <h1 class="likes_bloc-total">${totalLikes}</h1>
+        <i class="fa-solid fa-heart"></i>
+     </div>
+    `;
 
-    // INCREMENTE NOMBRE DE LIKES AU CLICK
-    let totalOfLikes = parseInt(document.getElementById("total-likes").innerText);
-    const likesArray = Array.from(document.querySelectorAll(".jaime")); // Créé un tableau de tous les <p class="jaime"></p>
-    likesArray.forEach((jaime) => { // boucle à travers chaque .jaime
-        let liked = false;
-        jaime.addEventListener("click", () => {
-            jaime.classList.toggle("red")
-            if(!liked){
-                jaime.previousElementSibling.innerText =
+  // INCREMENTE NOMBRE DE LIKES AU CLICK
+  let totalOfLikes = parseInt(document.querySelector('.likes_bloc-total').innerText);
+  const likesArray = Array.from(document.querySelectorAll('.like')); // Créé un tableau de tous les <p class="jaime"></p>
+  likesArray.forEach((jaime) => { // boucle à travers chaque .jaime
+    let liked = false;
+    jaime.addEventListener('click', () => {
+      if (!liked) {
+        jaime.previousElementSibling.innerText =
                 parseInt(jaime.previousElementSibling.innerText) + 1;
-                totalOfLikes += 1;
-                document.getElementById("total-likes").innerHTML = `${totalOfLikes}<i class="fa-solid fa-heart"></i>`;
-                liked = true;
-            }
-            else{
-                jaime.previousElementSibling.innerText =
+        totalOfLikes += 1;
+        document.querySelector('.likes_bloc-total').innerHTML = `${totalOfLikes}`;
+        liked = true;
+      } else {
+        jaime.previousElementSibling.innerText =
                 parseInt(jaime.previousElementSibling.innerText) - 1;
-                totalOfLikes -= 1;
-                document.getElementById("total-likes").innerHTML = `${totalOfLikes}<i class="fa-solid fa-heart"></i>`;
-                liked = false;
-            }
-        })
-    })
+        totalOfLikes -= 1;
+        document.querySelector('.likes_bloc-total').innerHTML = `${totalOfLikes}`;
+        liked = false;
+      }
+    });
+  });
 }
 
-totalLikes() 
+totalLikes();
 
