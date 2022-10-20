c0f0a0e8761f3ef3e916b55fec1a30ace45d54ee
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2022-10-20 11:47:00 +0200

    sort + likes

--- scripts/factories/medias.js
+++ scripts/factories/medias.js
@@ -19,7 +19,7 @@ class Media {
             <div class="medias_card-down--likes">
                 <p>${this.media.likes}</p>
                 <button class="like">
-                    <i class="visible-heart far fa-heart"></i>
+                    <i class="like far fa-heart"></i>
                 </button>
             </div>
         </div>
@@ -69,7 +69,8 @@ MediasWrapper = document.querySelector('.medias');
 
 
 class DisplayMedia {
-    constructor() {
+    constructor(photographers) {
+        this.photographers = photographers;
     }
 
     static async mainMedia() {
@@ -91,116 +92,76 @@ class DisplayMedia {
         const mediaWrapper =
             `
             <div class="sort_section">
-                <label id="listboxlabel" role="label" for="selected" name="Order by">Trier par</label>
+                <label>Trier par</label>
             </div>
 
             <div class="sort_list">
-                <button class="selected" id="selected" aria_labelledby="listboxlabel" aria-haspopup="listbox" aria-label="Populaire">
-                    Populaire
+                <button class="selected">Populaire
                     <i class="fas fa-chevron-down"></i>
                 </button>
                 <div class="options hidden">  
-                    <button class="optDate" role="option" aria-label="Date">Date</button>
-                    <button class="optTitle" role="option" aria-label="Titre">Titre</button>
+                    <button class="optDate">Date</button>
+                    <button class="optTitle">Titre</button>
                 </div>
             </div>
             `;
 
         wrapper.innerHTML = mediaWrapper;
         document.querySelector(".medias-sort").appendChild(wrapper)      
-    }
+    }   
 
-    static async mainTotalLikes() {
-        const mediasData = await getMedias();
-        const mediaFilter = mediasData.filter((media) => media.photographerId == photographerId);
+}
 
-        // Tableau qui regroupe tous les likes
-        let likes = mediaFilter.map((total) => total.likes); 
-        const initialValue = 0;
-        let totalLikes = likes.reduce((previousValue, currentValue) => previousValue + currentValue, initialValue);
-        document.querySelector('.likes').innerHTML +=
-        `<div class="likes_bloc">
-            <h1 class="likes_bloc-total">${totalLikes}</h1>
-            <i class="fa-solid fa-heart"></i>
-        </div>
-        `
-    
-        // INCREMENTE NOMBRE DE LIKES AU CLICK
-        let totalOfLikes = parseInt(document.querySelector('.likes_bloc-total').innerText);
-        let likesArray = Array.from(document.querySelectorAll('.like')); // Créé un tableau de tous les <p class="jaime"></p>
-        likesArray.forEach((jaime) => { // boucle à travers chaque .jaime
-            let liked = false;
-            jaime.addEventListener('click', () => {
-                jaime.classList.add("active")
-                if (!liked) {
-                jaime.previousElementSibling.innerText =
-                parseInt(jaime.previousElementSibling.innerText) + 1;
-                totalOfLikes += 1;
-                document.querySelector('.likes_bloc-total').innerHTML = `${totalOfLikes}`;
-                liked = true;
-                }
-                else {
-                jaime.previousElementSibling.innerText =
-                parseInt(jaime.previousElementSibling.innerText) - 1;
-                totalOfLikes -= 1;
-                document.querySelector('.likes_bloc-total').innerHTML = `${totalOfLikes}`;
-                liked = false;
-                }
-            });
+
+async function mainTotalLikes() {
+    const mediasData = await getMedias();
+    const photographersData = await getPhotographers();
+    const mediaFilter = mediasData.filter((media) => media.photographerId == photographerId);
+    const photographerFilter = photographersData.filter((photographer) => photographer.id == photographerId);
+    console.log(mediaFilter)
+
+    // Tableau qui regroupe tous les likes
+    let likes = mediaFilter.map((total) => total.likes); 
+    const initialValue = 0;
+    let totalLikes = likes.reduce((previousValue, currentValue) => previousValue + currentValue, initialValue);
+    document.querySelector('.likes').innerHTML +=
+    `<div class="likes_bloc">
+        <h1 class="likes-count likes_bloc-total">${totalLikes}</h1>
+        <i class="fa-solid fa-heart"></i>
+        <p>price</p>
+        <p class="likes_price">${this.price}€/jour</p>
+    </div>
+    `
+
+
+    // INCREMENTE NOMBRE DE LIKES AU CLICK
+    let totalOfLikes = parseInt(document.querySelector('.likes_bloc-total').innerText);
+    let likesArray = Array.from(document.querySelectorAll('.like')); // Créé un tableau de tous les <p class="jaime"></p>
+    likesArray.forEach((jaime) => { // boucle à travers chaque .jaime
+        let liked = false;
+        jaime.addEventListener('click', () => {
+            jaime.classList.toggle("fas")
+            if (!liked) {
+            jaime.previousElementSibling.innerText =
+            parseInt(jaime.previousElementSibling.innerText) + 1;
+            totalOfLikes += 1;
+            document.querySelector('.likes_bloc-total').innerHTML = `${totalOfLikes}`;
+            liked = true;
+            }
+            else {
+            jaime.previousElementSibling.innerText =
+            parseInt(jaime.previousElementSibling.innerText) - 1;
+            totalOfLikes -= 1;
+            document.querySelector('.likes_bloc-total').innerHTML = `${totalOfLikes}`;
+            liked = false;
+            }
         });
-    }
+    });
 }
 
 DisplayMedia.mainMedia()
-DisplayMedia.mainTotalLikes()
 DisplayMedia.createSortList()
+mainTotalLikes()
 
 
 
-
-
-
-/*static async sortMedia(){
-        const mediasData = await getMedias();
-        let mediaFilter = mediasData.filter((media) => media.photographerId == photographerId);
-        mediaFilter = mediaFilter.map((media) => MediaFactory.create(media)); // Afficher image ou video
-    
-        const select = document.getElementById('sort-select');
-
-        select.addEventListener('change', (e) => {
-            e.preventDefault();
-            const sortType = select.options[select.selectedIndex].id;
-    
-            // TRI PAR DATE
-            if (sortType === 'dateButton') {
-                mediaFilter.sort(dateFilter);
-                document.querySelector('.medias').innerHTML = '';
-                mediaFilter.forEach((media) => {
-                    MediasWrapper.appendChild(media.createMediaWrapper());
-                    Lightbox.init()                   
-                });
-            }
-
-            
-    
-            // TRI PAR POPULARITE
-            else if (sortType === 'popularityButton') {
-                mediaFilter.sort(popularityFilter);
-                document.querySelector('.medias').innerHTML = '';
-                mediaFilter.forEach((media) => {
-                    MediasWrapper.appendChild(media.createMediaWrapper());
-                    Lightbox.init()
-                });
-            }
-    
-            // TRI PAR TITRE
-            else if (sortType === 'titleButton') {
-                mediaFilter.sort(titleFilter);
-                document.querySelector('.medias').innerHTML = '';
-                mediaFilter.forEach((media) => {
-                    MediasWrapper.appendChild(media.createMediaWrapper());
-                    Lightbox.init()
-                });
-            }
-        });
-    }*/
--- scripts/utils/sorter.js
+++ scripts/utils/sorter.js
@@ -2,6 +2,7 @@ async function sortList() {
     const mediasData = await getMedias();
     let mediaFilter = mediasData.filter((media) => media.photographerId == photographerId);
     mediaFilter = mediaFilter.map((media) => MediaFactory.create(media)); // Afficher image ou video
+    
     const selectElement = document.querySelector(".selected");
     const options = document.querySelector(".options");
     const optDate = document.querySelector(".optDate");
@@ -13,74 +14,76 @@ async function sortList() {
         options.classList.toggle("hidden");
     });
 
-    if(optDate) {
+    if(optDate){
         optDate.addEventListener("click", () => {
             mediaFilter.sort(dateFilter);
                 document.querySelector('.medias').innerHTML = '';
                 mediaFilter.forEach((media) => {
-                    MediasWrapper.appendChild(media.createMediaWrapper());  
-                            
-            });
+                    MediasWrapper.appendChild(media.createMediaWrapper());                    
+                });
+                document.querySelector('.likes').innerHTML = ''
             refreshSortList("date");
-
         });
     }
-    if(optTitle) {
-        optTitle.addEventListener("click", () => {
 
+    if(optTitle){
+        optTitle.addEventListener("click", () => {
             mediaFilter.sort(titleFilter);
                 document.querySelector('.medias').innerHTML = '';
                 mediaFilter.forEach((media) => {
-                    MediasWrapper.appendChild(media.createMediaWrapper());  
-                  
-            });
+                    MediasWrapper.appendChild(media.createMediaWrapper());                
+                });
+                document.querySelector('.likes').innerHTML = ''
             refreshSortList("title");
         });
     }
-    if (optPopular) {
+
+    if(optPopular){
         optPopular.addEventListener("click", () => {
             mediaFilter.sort(popularityFilter);
                 document.querySelector('.medias').innerHTML = '';
                 mediaFilter.forEach((media) => {
-                    MediasWrapper.appendChild(media.createMediaWrapper());   
-                      
-            });
+                    MediasWrapper.appendChild(media.createMediaWrapper());                        
+                });
+                document.querySelector('.likes').innerHTML = ''
             refreshSortList("popular");
         });
     }
 }
 
-function manageSortList(opt){
+function organizeSortList(opt){
     const divSortList = document.querySelector(".sort_list");
     divSortList.innerHTML = " ";
     if (opt == "popular") {
         divSortList.innerHTML = 
-       `<button class="selected" id="selected" aria_labelledby="listboxlabel" aria-haspopup="listbox" aria-label="Populaire">Populaire<i class="fas fa-chevron-down"></i></button>
+       `<button class="selected">Populaire<i class="fas fa-chevron-down"></i></button>
        <div class="options hidden">  
-           <button class="optDate" role="option" aria-label="Date">Date</button>
-           <button class="optTitle" role="option" aria-label="Titre">Titre</button>
+           <button class="optDate">Date</button>
+           <button class="optTitle">Titre</button>
        </div>`;
     } else if (opt == "date") {
         divSortList.innerHTML = 
-       `<button class="selected" id="selected" aria_labelledby="listboxlabel" aria-haspopup="listbox" aria-label="Date">Date<i class="fas fa-chevron-down"></i></button>
+       `<button class="selected">Date<i class="fas fa-chevron-down"></i></button>
        <div class="options hidden">  
-           <button class="optPopular" role="option" aria-label="Populaire">Populaire</button>
-           <button class="optTitle" role="option" aria-label="Titre">Titre</button>
+           <button class="optPopular">Populaire</button>
+           <button class="optTitle">Titre</button>
        </div>`;
     } else if (opt == "title") {
         divSortList.innerHTML = 
-     `<button class="selected" id="selected" aria_labelledby="listboxlabel" aria-haspopup="listbox" aria-label="Titre">Titre<i class="fas fa-chevron-down"></i></button>
+     `<button class="selected">Titre<i class="fas fa-chevron-down"></i></button>
        <div class="options hidden">  
-           <button class="optPopular" role="option" aria-label="Populaire">Populaire</button>
-           <button class="optDate" role="option" aria-label="Date">Date</button>
+           <button class="optPopular">Populaire</button>
+           <button class="optDate">Date</button>
        </div>`;
     }
 }
 
 function refreshSortList(opt) {
-    manageSortList(opt);
+    organizeSortList(opt);
     Lightbox.init()   
     sortList();
+    mainTotalLikes()
+
 }
 
 function dateFilter(a, b) {
